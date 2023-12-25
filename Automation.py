8e2c3d30e6c6fba4ba9a84adc165a1677514b628
Mainak <114569441+toomanybugss@users.noreply.github.com>
2023-12-25 12:30:44 +0530

    Add files via upload

---
+++ Automation.py
@@ -0,0 +1,8 @@
+from AppOpener import open
+import webbrowser
+open("whatsapp,visual studio code,notepad,microsoft edge,Notepad,Notion,file explorer")
+
+webbrowser.open('www.youtube.com', new = 0, autoraise = True)
+webbrowser.open('https://calendar.google.com/calendar/u/0/r?pli=1', new = 0, autoraise = True)
+webbrowser.open('https://chat.openai.com/', new = 0, autoraise = True)
+webbrowser.open('https://www.youtube.com/watch?v=b3TOVBNSJDA&t=1735s&ab_channel=nobody', new = 0, autoraise = True)
